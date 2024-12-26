6764fd7798742c5b19476786dd0f168f38ecbab8
shinraTenseinn <alonesushant9@gmail.com>
2024-12-26 21:52:27 +0530

    Add files via upload
    First Commit

---
+++ SwiftUIdentity/SwiftUIdentity/SwiftUIdentityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIdentityApp.swift
+//  SwiftUIdentity
+//
+//  Created by Nine Tails on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIdentityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
