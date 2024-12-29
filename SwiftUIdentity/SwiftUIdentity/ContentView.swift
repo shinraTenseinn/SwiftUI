deabe9fb084c0a77809a6c0ce35c16ca60887b83
Nine Tails <alonesushant9@gmail.com>
2024-12-29 22:41:01 +0530

    Without ID

--- SwiftUIdentity/SwiftUIdentity/ContentView.swift
+++ SwiftUIdentity/SwiftUIdentity/ContentView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var firstCounter = 1
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            FirstView()
+        }
+        .padding()
+    }
+}
+
+struct FirstView: View {
+    @State var firstCounter = 1
+    var body: some View {
+        VStack {
+            Button(action: {
+                firstCounter += 1
+            }, label: {
+                Text("First Counter \(firstCounter)")
+            })
+            SecondView()
+        }
+        .padding()
+    }
+}
+
+struct SecondView: View {
+    @State var secondCounter = 1
+    var body: some View {
+        VStack {
+            Button(action: {
+                secondCounter += 1
+            }, label: {
+                Text("Second Counter \(secondCounter)")
+            })
         }
         .padding()
     }
